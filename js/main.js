00aa0bb8df2a358c87fcdb14b545246918187f37
fatima <fatima.jacoboruiz@gmail.com>
2016-09-15 12:22:39 -0500

    bicis form

--- js/main.js
+++ js/main.js
@@ -3,14 +3,17 @@ function validateForm(){
 	var apellido = document.getElementById("lastname").value;
 	var correo = document.getElementById("input-email").value;
 	var contraseña = document.getElementById("input-password").value;
+	var lista = document.getElementsByTagName('select')[0].selectedIndex;
 
 	if( nombre == null || nombre.length == 0 || /^[A-Z]\s+$/.test(nombre)) {
-		return alert('Ingresa tu Nombre');	
+		alert('Ingresa tu Nombre');	
 	} else if (apellido == null || apellido.length == 0 || /^[A-Z]\s+$/.test(apellido)){
-    	return alert('Ingresa tu Apellido');
-  	} else if ( !(/\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)/.test(correo))){
-  		return alert('Ingresa tu correo electrónico');
-  	} else if (contraseña == null || contraseña.length == 0 || /^\s+$/.test(contraseña)){
-    	return alert('Ingresa tu contraseña');
-    }
+    	alert('Ingresa tu Apellido');
+  	} else if(!(/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(correo))) {
+        alert("Ingresa tu email");
+  	} else if (contraseña == null || contraseña.length <=6 || contraseña == "password" || contraseña == 123456 || contraseña == 098754 || /^\s+$/.test(contraseña)){
+    	alert('Ingresa una contraseña válida');
+   	} else if ( lista == 'blank' ){
+    	alert('Selecciona una opción!');
+   	}
 }
